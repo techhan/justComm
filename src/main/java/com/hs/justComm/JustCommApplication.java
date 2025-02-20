8464029160b045aff9a5ef238f207c88180721fd
techhan <devhan9117@gmail.com>
2025-02-20 21:38:33 +0900

    springboot project init

---
+++ src/main/java/com/hs/justComm/JustCommApplication.java
@@ -0,0 +1,13 @@
+package com.hs.justComm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustCommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustCommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hs/justComm/JustCommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.justComm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustCommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
