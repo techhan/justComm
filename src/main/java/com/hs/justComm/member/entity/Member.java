7dc42575dc67f5dde381bd5ba8ff3630df0b7c1e
techhan <devhan9117@gmail.com>
2025-02-24 22:50:51 +0900

    회원가입 기능 토대 개발

---
+++ src/main/java/com/hs/justComm/member/entity/Member.java
@@ -0,0 +1,28 @@
+package com.hs.justComm.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private String password;
+    private String nickname;
+    private String email;
+    private String job;
+    private int age;
+}
